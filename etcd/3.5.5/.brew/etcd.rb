4b146511902403bb7b4d2f3de5a5d493b23d4895
김초원 <zzzcc33@naver.com>
2023-09-24 18:02:41 +0900

    install ETCD

---
+++ etcd/3.5.5/.brew/etcd.rb
@@ -0,0 +1,58 @@
+class Etcd < Formula
+  desc "Key value store for shared configuration and service discovery"
+  homepage "https://github.com/etcd-io/etcd"
+  url "https://github.com/etcd-io/etcd.git",
+      tag:      "v3.5.5",
+      revision: "19002cfc689fba2b8f56605e5797bf79f8b61fdd"
+  license "Apache-2.0"
+  head "https://github.com/etcd-io/etcd.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "build"
+    bin.install Dir[buildpath/"bin/*"]
+  end
+
+  plist_options manual: "etcd"
+
+  service do
+    environment_variables ETCD_UNSUPPORTED_ARCH: "arm64" if Hardware::CPU.arm?
+    run [opt_bin/"etcd"]
+    run_type :immediate
+    keep_alive true
+    working_dir var
+  end
+
+  test do
+    test_string = "Hello from brew test!"
+    etcd_pid = fork do
+      if OS.mac? && Hardware::CPU.arm?
+        # etcd isn't officially supported on arm64
+        # https://github.com/etcd-io/etcd/issues/10318
+        # https://github.com/etcd-io/etcd/issues/10677
+        ENV["ETCD_UNSUPPORTED_ARCH"]="arm64"
+      end
+
+      exec bin/"etcd",
+        "--enable-v2", # enable etcd v2 client support
+        "--force-new-cluster",
+        "--logger=zap", # default logger (`capnslog`) to be deprecated in v3.5
+        "--data-dir=#{testpath}"
+    end
+    # sleep to let etcd get its wits about it
+    sleep 10
+
+    etcd_uri = "http://127.0.0.1:2379/v2/keys/brew_test"
+    system "curl", "--silent", "-L", etcd_uri, "-XPUT", "-d", "value=#{test_string}"
+    curl_output = shell_output("curl --silent -L #{etcd_uri}")
+    response_hash = JSON.parse(curl_output)
+    assert_match(test_string, response_hash.fetch("node").fetch("value"))
+
+    assert_equal "OK\n", shell_output("#{bin}/etcdctl put foo bar")
+    assert_equal "foo\nbar\n", shell_output("#{bin}/etcdctl get foo 2>&1")
+  ensure
+    # clean up the etcd process before we leave
+    Process.kill("HUP", etcd_pid)
+  end
+end
